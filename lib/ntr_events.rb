6ebed4918b81a8906d16bc90817f222390e7ab84
thesilentc <thesilentc@me.com>
2017-05-10 15:49:50 -0400

    initial Gem commit

---
+++ lib/ntr_events.rb
@@ -0,0 +1,5 @@
+require "ntr_events/version"
+
+module NtrEvents
+  # Your code goes here...
+end
---
+++ lib/ntr_events/cli.rb
@@ -0,0 +1,43 @@
+# CLI controller
+class NtrEvents::CLI
+
+  def call
+   puts "Welcome to National Team Roping Events"
+   NtrEvents::Scraper.new.scrape_events
+   list_events
+   menu
+  end
+
+ def list_events
+   NtrEvents::Event.all.each_with_index do |event, i|
+     puts "#{i+1} #{event.name}"
+   end
+ end
+
+ def menu
+   input = ""
+   while input != "exit"
+     puts "What story do you want to read?"
+     input = gets.strip
+
+     if input.to_i-1 <= TechcrunchCli::Story.all.size
+       story = TechcrunchCli::Story.all[input.to_i-1]
+
+       puts story.title
+       puts
+       puts "By: #{story.author}"
+       puts "Summary: #{story.summary}"
+
+       puts "Would you like to read more?"
+       answer = gets.strip
+
+       if ["Y", "YES"].include?(answer.upcase)
+         # how do I get the content for this story?
+         story.open_in_browser
+       end
+     end
+     puts "Would you like to exit or list again?"
+     input = gets.strip
+   end
+ end
+end
